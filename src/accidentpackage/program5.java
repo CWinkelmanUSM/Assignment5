d5b7c6c1cef765c686e3dcdbdf410b1319d1b4d0
Charles Winkelman <charles.winkelman@maine.edu>
2024-03-03 17:22:38 -0500

    Changed main and build test function to build trees

--- src/accidentpackage/program5.java
+++ src/accidentpackage/program5.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -48,35 +49,45 @@ public class program5 {
 	}
 	
 	
+	public static void test(String filePath) {
+		HashMap<String, myBST> states = new HashMap<>();
+		try (FileReader fileReader = new FileReader(filePath)) {
+			BufferedReader reader = new BufferedReader(fileReader);
+			String line;
+			String[] data;
+			reader.readLine();
+			while ((line = reader.readLine()) != null) {
+				data = line.split(",");
+				AccidentRecord entry = new AccidentRecord(data[0], Integer.parseInt(data[1]), data[2], data[3],
+							data[4], data[5], data[6], data[7], Double.parseDouble(data[8]),
+							Double.parseDouble(data[9]), Double.parseDouble(data[10]), data[11], data[12]);
+				if (states.containsKey(entry.getState())) {
+					states.get(entry.getState()).add(entry);
+				} else {
+					states.put(entry.getState(), new myBST(entry));
+				}
+				int total = 0;
+				
+				for (myBST tree : states.values()) {
+					System.out.println("Nodes:" + tree.size());
+					total += tree.size();
+				}
+				System.out.println("Total: " + total);
+			}
+		} catch (IOException e) {
+				e.printStackTrace();
+		}
+	}
+	
 	// Unimplemented functions
 	
 	public static void main(String[] args) {
-		// String[] args2 = { "CA", "Los Angeles", "accidents.csv" }; reference list. I
-		// was not able to test the program via command prompt.
-		Long startTime;
-		Long endTime;
-		String state = args[0];
-		String city = args[1];
-		String fileName = args[2];
-
-		List<AccidentRecord> Report = new ArrayList<>();
-
-		startTime = System.nanoTime();
-		Report = reader(state, city, fileName);
-		endTime = System.nanoTime();
-		System.out.println((endTime - startTime) / 1000000000.0 + " seconds to read the records");
-
-		if (Report.isEmpty() != true) {
-
-			Comparator<AccidentRecord> byStartTime = (s1, s2) -> s1.getStartTime().compareTo(s2.getStartTime());
-			startTime = System.nanoTime();
-			Collections.sort(Report, byStartTime);
-			endTime = System.nanoTime();
-
-
-		} else {
-			System.out.println("No Records Found");
-		}
-
+		Timer t = new Timer();
+		
+		t.start();
+		test("resources/accidents_small_sample.csv");
+		t.stop();
+		
+		System.out.println("Built in " + t.getTime());
 	}
 }
